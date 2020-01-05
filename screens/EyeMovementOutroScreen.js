f6f47f0e3dec27294ee0d552bee56340e5a01c0e
Keira Schoolcraft <keira.schoolcraft@icloud.com>
2020-01-05 18:14:38 -0500

    Linked to google survey at end of eye movement successfully

--- screens/EyeMovementOutroScreen.js
+++ screens/EyeMovementOutroScreen.js
@@ -8,24 +8,41 @@ import {
   TouchableOpacity,
   View,
   Button,
+  Linking,
 } from 'react-native';
+
+
+
 
 class EyeMovementOutroScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
                 <Text style={styles.infoText}> Thanks for completing the eye movement activity!</Text>
+                <Text style={styles.infoText}> Please take the Google Survey as linked below.</Text>
                 <View style={styles.buttonContainer}>
+                    <Button
+                        title="Google Survey"
+                        onPress={this._handleOpenWithLinking}
+                        style={styles.button}
+                        color='white'
+                    />
                     <Button
                         onPress={() => this.props.navigation.navigate('Home')}
                         title="Take Me Home"
-                        color = 'white'
+                        color='white'
                     />
                 </View>
             </View>
         );
     }
+    _handleOpenWithLinking = () => {
+        Linking.openURL('https://docs.google.com/forms/d/e/1FAIpQLSeyAJ31zEabP8yR9jriO8l3HIW8gAQEKCDG7w92kA__HlBMhQ/viewform?usp=sf_link');
+    }
+
 }
+
+
 
 const styles = StyleSheet.create({
     container: {
@@ -33,14 +50,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#49f',
       },
       buttonContainer: {
-        flex: 2,
+        flex: 5,
         textAlign: 'center',
         justifyContent: 'space-around',
         flexDirection: 'column',
       },
       infoText: {
-          fontSize: 35,
-          flex: 2,
+          fontSize: 25,
+          flex: 1,
           textAlign: 'center',
           justifyContent: 'space-around',
           padding: 80,
--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -9,7 +9,7 @@ export default function LinksScreen() {
        * Go ahead and delete ExpoLinksView and replace it with your content;
        * we just wanted to provide you with some helpful links.
        */}
-      <https://docs.google.com/forms/d/e/1FAIpQLSeyAJ31zEabP8yR9jriO8l3HIW8gAQEKCDG7w92kA__HlBMhQ/viewform?vc=0&c=0&w=1/>
+      <ExpoLinksView />
     </ScrollView>
   );
 }
