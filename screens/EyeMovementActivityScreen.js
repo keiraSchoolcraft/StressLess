5ce1fe05f99360a837ef554769970312c6a64c67
Keira Schoolcraft <keira.schoolcraft@icloud.com>
2020-01-07 18:00:19 -0500

    Added the new pictures to the eye movement activity.

--- screens/EyeMovementActivityScreen.js
+++ screens/EyeMovementActivityScreen.js
@@ -25,63 +25,56 @@ class ShowTime extends React.Component {
   class DotPictureArray extends React.Component {
     render () {
       let pics = [
-        <Image source={require('../assets/images/dots1a.png')} />,
-        <Image source={require('../assets/images/dots2a.png')} />,
-        <Image source={require('../assets/images/dots3a.png')} />,
-        <Image source={require('../assets/images/dots4a.png')} />,
-        <Image source={require('../assets/images/dots5a.png')} />,
-        <Image source={require('../assets/images/dots6a.png')} />,
-        <Image source={require('../assets/images/dots7a.png')} />,
-        <Image source={require('../assets/images/dots8a.png')} />,
-        <Image source={require('../assets/images/dots9a.png')} />,
-        <Image source={require('../assets/images/dots10a.png')} />,
-        <Image source={require('../assets/images/dots11a.png')} />,
-        <Image source={require('../assets/images/dots12a.png')} />,
-        '',
-        '',
-        '',
-        '',
-        '',
-        <Image source={require('../assets/images/dots1b.png')} />,
-        <Image source={require('../assets/images/dots2b.png')} />,
-        <Image source={require('../assets/images/dots3b.png')} />,
-        <Image source={require('../assets/images/dots4b.png')} />,
-        <Image source={require('../assets/images/dots5b.png')} />,
-        <Image source={require('../assets/images/dots6b.png')} />,
-        <Image source={require('../assets/images/dots7b.png')} />,
-        <Image source={require('../assets/images/dots8b.png')} />,
-        <Image source={require('../assets/images/dots9b.png')} />,
-        <Image source={require('../assets/images/dots10b.png')} />,
-        <Image source={require('../assets/images/dots11b.png')} />,
-        <Image source={require('../assets/images/dots12b.png')} />,
-        '',
-        '',
-        '',
-        '',
-        '',
-        <Image source={require('../assets/images/dots1c.png')} />,
-        <Image source={require('../assets/images/dots2c.png')} />,
-        <Image source={require('../assets/images/dots3c.png')} />,
-        <Image source={require('../assets/images/dots4c.png')} />,
-        <Image source={require('../assets/images/dots5c.png')} />,
-        <Image source={require('../assets/images/dots6c.png')} />,
-        <Image source={require('../assets/images/dots7c.png')} />,
-        <Image source={require('../assets/images/dots8c.png')} />,
-        <Image source={require('../assets/images/dots9c.png')} />,
-        <Image source={require('../assets/images/dots10c.png')} />,
-        <Image source={require('../assets/images/dots11c.png')} />,
-        <Image source={require('../assets/images/dots12c.png')} />,
+        '../assets/images/dots1a.png',
+        '../assets/images/dots2a.png',
+        '../assets/images/dots3a.png',
+        '../assets/images/dots4a.png',
+        '../assets/images/dots5a.png',
+        '../assets/images/dots6a.png',
+        '../assets/images/dots7a.png',
+        '../assets/images/dots8a.png',
+        '../assets/images/dots9a.png',
+        '../assets/images/dots10a.png',
+        '../assets/images/dots11a.png',
+        '../assets/images/dots12a.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/dots1b.png',
+        '../assets/images/dots2b.png',
+        '../assets/images/dots3b.png',
+        '../assets/images/dots4b.png',
+        '../assets/images/dots5b.png',
+        '../assets/images/dots6b.png',
+        '../assets/images/dots7b.png',
+        '../assets/images/dots8b.png',
+        '../assets/images/dots9b.png',
+        '../assets/images/dots10b.png',
+        '../assets/images/dots11b.png',
+        '../assets/images/dots12b.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/blank.png',
+        '../assets/images/dots1c.png',
+        '../assets/images/dots2c.png',
+        '../assets/images/dots3c.png',
+        '../assets/images/dots4c.png',
+        '../assets/images/dots5c.png',
+        '../assets/images/dots6c.png',
+        '../assets/images/dots7c.png',
+        '../assets/images/dots8c.png',
+        '../assets/images/dots9c.png',
+        '../assets/images/dots10c.png',
+        '../assets/images/dots11c.png',
+        '../assets/images/dots12c.png',
       ]
   
       return (
-        <Text 
-        style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            width: 300,
-            height: 300,
-        }} >
-            {pics[this.props.currentCycle]}</Text>
+        <Image source={pics[index]}/>
       )
     }
   }
@@ -123,32 +116,18 @@ class EyeMovementActivityScreen extends React.Component {
             console.log(this.state);
         }
     }
-  render() {
-    return (
-        <View style={styles.container}>
-        <Text style={styles.title}>Eye Movement</Text>
-        {/* <View style={styles.wrapper}>
-          <DoThing currentCycle={this.state.currentCycle} timeInCycle={this.state.timeInCycle} />
-        </View> */}
-        
-        <View style={styles.wrapper}>
-          <DotPictureArray currentCycle={this.state.currentCycle} />
-        </View>
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Eye Movement</Text>
 
-        {/* <View style={styles.wrapper}>
-          <ShowTime label={'Current Cycle'} currentTime={this.state.currentCycle} />
-        </View> */}
+                <View style={styles.wrapper}>
+                    <DotPictureArray currentCycle={this.state.currentCycle} />
+                </View>
 
-        <View style={styles.wrapper}>
-          <ShowTime label={'Time in Cycle'} currentTime={this.state.timeInCycle} />
-        </View>
-
-        <View style={styles.wrapper}>
-          <ShowTime label={'Time Overall'} currentTime={this.state.timeOverall} />
-        </View>
-      </View>
-    );
-  }
+            </View>
+        );
+    }
 }
 
 
